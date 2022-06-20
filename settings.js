7e5a22aaff7e71f4e74bdc2d9266c4e420152793
tsbot99 <107861708+tsbot99@users.noreply.github.com>
2022-06-20 18:54:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94726140613']
+global.premium = ['94726140613']
+global.ownernomer = '94726140613'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
